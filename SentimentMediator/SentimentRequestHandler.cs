c53c77b7053522f2782f39393ad2bf57ca480564
crisamaro24 <cristianamaro7@gmail.com>
2019-04-06 13:25:28 -0400

    Version 0.1.0

---
+++ SentimentMediator/SentimentRequestHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using SentimentMediator.ViewModels;
+using SentimentServices.Services;
+
+namespace SentimentMediator
+{
+    public class SentimentRequestHandler : IRequestHandler<SentimentRequest, SentimentResponse>
+    {
+        public async Task<SentimentResponse> Handle(SentimentRequest request, CancellationToken cancellationToken)
+        {
+            return await Task.Run(() =>
+                new SentimentResponse
+                {
+                    Message = request.Message,
+                    Score = SentimentService.Predict(request.Message).Percentage
+                }
+            );
+        }
+    }
+}
---
+++ SentimentServices/Services/SentimentService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using Microsoft.ML;
+using SentimentServices.Models;
+
+namespace SentimentServices.Services
+{
+    public static class SentimentService
+    {
+        static MLContext context = new MLContext();
+        static ITransformer model
+            = context.Model.Load(File.Open("C:/Users/amaro/source/repos/SentimentAnalysisApi/SentimentAnalysisApi/SentimentModel.zip", FileMode.Open));
+
+        [ThreadStatic]
+        static PredictionEngine<SourceData, Prediction> t_engine;
+
+        private static PredictionEngine<SourceData, Prediction> GetPredictionEngine()
+        {
+            if (t_engine != null)
+                return t_engine;
+
+            return t_engine = model.CreatePredictionEngine<SourceData, Prediction>(context);
+        }
+
+        public static Prediction Predict(string text)
+        {
+            var engine = GetPredictionEngine();
+            return engine.Predict(new SourceData { SentimentText = text });
+        }
+    }
+}
